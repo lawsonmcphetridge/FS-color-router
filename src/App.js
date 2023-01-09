2fa47608f470dff5c05ed3129a7961debc65eb5f
Lawson <lawsonmcphetridge2020@gmail.com>
2023-01-09 15:57:37 -0800

    ci fix

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
 import NotFound from './NotFound/NotFound';
 import Home from './Home/Home';
 
-
-
-
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header></Header>
+      <Routes>
+        <Route path="/rgb/:red/:green/:blue" element={<Main />} />
 
-      <Switch>
+        <Route path="/" element={<Home />} />
 
-        <Route path='/rgb/:red/:green/:blue'>
-          <Main />
-        </Route>
-
-
-        <Route exact path='/'>
-          <Home />
-        </Route>
-
-        <Route path='*'>
-          <NotFound />
-        </Route>
-        
-
-
-
-      </Switch>
-      
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: ['./src/index.js'],
+  output: {
+    publicPath: '/',
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js',
+    clean: true,
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', { loader: 'css-loader' }],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
+};
